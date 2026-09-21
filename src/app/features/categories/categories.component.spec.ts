a0a78e09c4a00f5bec8eccccbb94afea0a9a1437
Hamza Abdullahi <alphaxamze@gmail.com>
2026-09-21 22:12:37 +0300

    test(f1.1): fix HttpClient test providers and AppComponent Hardware PRO specs

--- src/app/features/categories/categories.component.spec.ts
+++ src/app/features/categories/categories.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { CategoriesComponent } from './categories.component';
 
@@ -8,9 +10,12 @@ describe('CategoriesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CategoriesComponent]
-    })
-    .compileComponents();
+      imports: [CategoriesComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CategoriesComponent);
     component = fixture.componentInstance;
