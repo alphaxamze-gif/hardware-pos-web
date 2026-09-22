762c7c198cfd9a53a1ca8bfeb547dfa64fddab06
alphaxamze-gif <alphaxamze@gmail.com>
2026-09-22 16:23:00 +0300

    feat(frontend): add centralized API environment configuration

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject, tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private apiUrl = `${environment.apiBaseUrl}/api/auth`;
   private currentUserSubject = new BehaviorSubject<any>(null);
   currentUser$ = this.currentUserSubject.asObservable();
 
--- src/app/core/services/customer.service.ts
+++ src/app/core/services/customer.service.ts
@@ -6,28 +6,28 @@ import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
-  private apiUrl = `${environment.apiBaseUrl}/api/categories`;
+export class CustomerService {
+  private apiUrl = `${environment.apiBaseUrl}/api/customers`;
 
   constructor(private http: HttpClient) {}
 
-  getCategories(): Observable<any[]> {
+  getCustomers(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
-  getCategory(id: string): Observable<any> {
+  getCustomer(id: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 
-  createCategory(data: any): Observable<any> {
+  createCustomer(data: any): Observable<any> {
     return this.http.post<any>(this.apiUrl, data);
   }
 
-  updateCategory(id: string, data: any): Observable<any> {
+  updateCustomer(id: string, data: any): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/${id}`, data);
   }
 
-  deleteCategory(id: string): Observable<any> {
+  deleteCustomer(id: string): Observable<any> {
     return this.http.delete<any>(`${this.apiUrl}/${id}`);
   }
 }
